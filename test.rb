e26d8405af4cf4d3894861e3b8bd1433fe3e0b59
takeru tsuda <da_tsuudayo@yahoo.co.jp>
2019-09-20 06:24:15 +0000

    ウェブ技術課題を提出

---
+++ test.rb
@@ -0,0 +1,29 @@
+require 'webrick'
+
+module WEBrick
+  module HTTPServlet
+    FileHandler.add_handler('rb', CGIHandler)
+  end
+end
+
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+
+# WEBrick::HTTPServlet::FileHandlerをWEBrick::HTTPServlet::ERBHandlerに変更する
+# 'test.html'を'test.html.erb'に変更する
+
+
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+
+# この一行を追記
+# server.mount('/goya_quality.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya_quality.rb')
+
+server.start
